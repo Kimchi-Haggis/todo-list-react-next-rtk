d75f96aee85f7891a3f32d869900948045cac887
Kimchi-Haggis <kimchihaggis@gmail.com>
2022-08-26 14:58:05 +0100

    [Docs] cleanup comments (#8)

--- components/todo/TodoList.tsx
+++ components/todo/TodoList.tsx
@@ -1,29 +1,16 @@
-// features/todos/TodoList.tsx
+import React from "react";
 
 import { selectTodos, toggleTodo } from "@redux/slice/todosSlice";
 import { RootState } from "@redux/store";
-import React from "react";
-import {
-  useSelector,
-  useDispatch,
-  TypedUseSelectorHook 
-} from 'react-redux'
+import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux'
 
-// Import selector and action from slice:
-
-// We use RootState we defined earlier
-// to make `useSelector` understand 
-// the store structure via type assertion 
-// with the `TypedUseSelectorHook` generic:
-// const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const TodoList = () => {
   const dispatch = useDispatch();
-  // Now, use the selector inside right away,
-  // no need to destructure the result:
-  const todos = useSelector(selectTodos);
 
-  // The rest of the code stays the same:
+  const todos = useTypedSelector(selectTodos);
+
   return (
     <ul>
       {todos.map((todo) => (
